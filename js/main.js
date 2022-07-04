fede4ce4dbf987d3417d89a32d8e8ea9baed2691
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-04 17:22:03 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ let prezzo = chilometri * 0.21;
 
 const eta = parseInt(prompt('Inserisci la tua età'));
 if (eta < 18) {
-    prezzo = ((prezzo * 20) / 100);
+    let sconto = ((prezzo * 20) / 100);
+    prezzo = (prezzo - sconto);
     alert('Hai diritto al 20% di sconto!')
     alert(`Il prezzo del tuo biglietto è di ${prezzo.toFixed(2)} Euro`);
 } else if (eta > 65) {
-    prezzo = ((prezzo * 40) / 100);
+    sconto = ((prezzo * 40) / 100);
+    prezzo = (prezzo - sconto);
     alert('Hai diritto al 40% di sconto!')
     alert(`Il prezzo del tuo biglietto è di ${prezzo.toFixed(2)} Euro`);
 } else {
