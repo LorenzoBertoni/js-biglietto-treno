a9b2070018c379203bf280f5843ead8ba7dedd70
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-04 15:57:35 +0200

    fix: rimossa la variabile "sconto" e inserito il suo valore nella variabile "prezzo" così da rimuovere un passaggio nel programma

--- js/main.js
+++ js/main.js
@@ -3,13 +3,11 @@ let prezzo = chilometri * 0.21;
 
 const eta = parseInt(prompt('Inserisci la tua età'));
 if (eta < 18) {
-    let sconto = ((prezzo * 20) / 100);
-    prezzo = (prezzo - sconto);
+    prezzo = ((prezzo * 20) / 100);
     alert('Hai diritto al 20% di sconto!')
     alert(`Il prezzo del tuo biglietto è di ${prezzo.toFixed(2)} Euro`);
 } else if (eta > 65) {
-    sconto = ((prezzo * 40) / 100);
-    prezzo = (prezzo - sconto);
+    prezzo = ((prezzo * 40) / 100);
     alert('Hai diritto al 40% di sconto!')
     alert(`Il prezzo del tuo biglietto è di ${prezzo.toFixed(2)} Euro`);
 } else {
